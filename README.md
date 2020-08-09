4bbcf85821f45eafd264bd4617c1c6a152521ce3
Luiz Fernando Duarte Junior <luizfduartejr@MacBook-Pro-de-Luiz.local>
2020-08-09 18:55:49 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# crud-react-native
+A simple mobile CRUD built with React Native.
+
+Run 'npm install' in the app folder.
+
+Run 'npm start'in the app folder.
---
+++ app/AppList.js
@@ -0,0 +1,60 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import AppItem from './AppItem';
+import AsyncStorage from '@react-native-community/async-storage';
+ 
+export default function AppList() {
+  const [items, setItems] = useState([]);
+  
+  useEffect(() => {
+      AsyncStorage.getItem('items')
+        .then(response => {
+            if(response)
+                setItems(JSON.parse(response));
+        })
+  }, items); 
+
+  return (
+    <View style={styles.container}>
+        <StatusBar style="light" />
+        <Text style={styles.title}>Lista de Compras</Text>
+        <ScrollView 
+            style={styles.scrollContainer}
+            contentContainerStyle={styles.itemsContainer}>
+            { items.map(item => {
+                return <AppItem key={item.id} item={item.quantidade + '  de ' + item.descricao} />
+            }) }
+        </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D93600',
+    alignItems: 'center',
+    justifyContent: 'center',
+    //height: '100%'
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 20
+  },
+  scrollContainer: {
+    flex: 1,
+    width: '90%'
+  },
+  itemsContainer: {
+    marginTop: 10,
+    padding: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    alignItems: 'stretch',
+    backgroundColor: '#fff'
+  },
+});
